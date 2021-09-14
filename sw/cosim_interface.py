d3d91c216b3cb6e8359fe6aec51d1288bcd402a8
Michał Kruszewski <mkru@protonmail.com>
2021-09-14 13:10:53 +0200

    Add docs to cosim_interface.py.

--- sw/cosim_interface.py
+++ sw/cosim_interface.py
@@ -3,6 +3,19 @@ import logging as log
 
 class CosimInterface:
     def __init__(self, write_fifo_path, read_fifo_path, delay_function=None, delay=False):
+        """Create co-simulation interface.
+        Parameters:
+        -----------
+        write_fifo_path
+            Path to software -> firmware named pipe.
+        read_fifo_path
+            Path to firmware -> software named pipe.
+        delay_function
+            Reference to function returning random value when delay is set to 'True'.
+        delay
+            If set to 'True' there is a random delay between any write or read operation.
+            Useful for modelling real access times.
+        """
         self.write_fifo_path = write_fifo_path
         self.read_fifo_path = read_fifo_path
 
@@ -17,12 +30,14 @@ class CosimInterface:
         self.delay = delay
 
     def _make_fifos(self):
+        """Create named pipes needed for inter-process communication."""
         self._remove_fifos()
         log.info("Making FIFOs")
         os.mkfifo(self.write_fifo_path)
         os.mkfifo(self.read_fifo_path)
 
     def _remove_fifos(self):
+        """Remove named pipes."""
         try:
             log.info("Removing FIFOs")
             os.remove(self.write_fifo_path)
@@ -31,6 +46,14 @@ class CosimInterface:
             pass
 
     def write(self, addr, val):
+        """Write register.
+        Parameters
+        ----------
+        addr
+            Register address.
+        val
+            Value to be written.
+        """
         if self.delay:
             self.wait(self.delay_function())
 
@@ -47,6 +70,13 @@ class CosimInterface:
             raise Exception("Wrong status returned:" + s.strip())
 
     def read(self, addr):
+        """Read register.
+
+        Parameters
+        ----------
+        addr
+            Register address.
+        """
         if self.delay:
             self.wait(self.delay_function())
 
@@ -66,6 +96,12 @@ class CosimInterface:
         return val
 
     def wait(self, time_ns):
+        """Wait in simulator for given amount of time.
+        Parameters
+        ----------
+        time_ns
+            Time to wait in nanoseconds.
+        """
         assert time_ns > 0 , "Wait time must be greater than 0"
 
         log.info("Waiting for %d ns" % time_ns)
@@ -81,6 +117,12 @@ class CosimInterface:
             raise Exception("Wrong status returned:" + s.strip())
 
     def end(self, status):
+        """End co-simulation with given status.
+        Parameters:
+        -----------
+        status
+            Status to be returned by the simulation process.
+        """
         log.info("Ending with status %d" % status)
 
         cmd = "E" + ("%.8x" % status) + "\n"
